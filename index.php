8092cd4a0682266db620b9c2a6bf749a3a6d764a
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-03-21 01:14:20 -0400

    added bootstrap css and style.css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 			dynamicChat();
 		}, 1000);
 	</script>
-    <link href="bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
